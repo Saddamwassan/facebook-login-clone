2859d3d4edb989bc4a5baa244d5011caa04a7681
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-01-09 22:08:07 +0500

    facebook-login
    facebook login page created.

---
+++ script.js
@@ -0,0 +1,16 @@
+function signUpForm(){
+    var x = document.getElementById("main");
+    x.style.display= "block";
+}
+function closeForm(){
+    var x = document.getElementById("main");
+    x.style.display= "none";
+}
+function hideCustom(){
+    var x = document.querySelector(".custom-main")
+    x.style.display="none";
+}
+function showCustom(){
+    var x = document.querySelector(".custom-main")
+    x.style.display="block";
+}
